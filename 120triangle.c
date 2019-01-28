ee75050a9196e71f9e63d0199043e916ce533d4d
shaggyday <43423101+shaggyday@users.noreply.github.com>
2019-01-28 16:47:51 -0600

    Add files via upload

--- 120triangle.c
+++ 120triangle.c
@@ -9,38 +9,22 @@ int YCoordOf(int x0, double slope, double a0, double a1){
     return (int)(slope*x0 - slope*a0 + a1);
 }
 
-/* Helper function to rasterize */
-void aggregateArray(int varyDim, double points[],const double a[], const double b[],const double c[]){
-    int i;
-    for(i = 0;i < varyDim; i = i + 1){
-        points[i] = a[i];
-        points[i+varyDim] = b[i];
-        points[i+2*varyDim] = c[i];
-    } 
-}
-
-/* Helper function to create three new vertix arrays */
-void distributeArray(int varyDim, double points[], double aPrime[],  double bPrime[], double cPrime[]){
-    int i;
-    for(i = 0;i < varyDim; i = i + 1){
-        aPrime[i] = points[i];
-        bPrime[i] = points[i+varyDim];
-        cPrime[i] = points[i+2*varyDim];
-    } 
-}
-
 /* Sort the three vertices in the correct order */
-void rasterize(int varyDim, double points[], const double a[], const double b[],const double c[]){
-    if ((c[0]<=b[0] && b[0]<=a[0])||
-        (c[0]<=a[0] && a[0]<=b[0] && a[0]!=c[0])){
-        aggregateArray(varyDim,points,c,a,b);
+void rasterize(int varyDim, const double a[], const double b[], const double c[],double aPrime[],double bPrime[],double cPrime[]){
+    if((c[0]<a[0] && c[0]<b[0]) || (a[0]==b[0]&&c[0]<a[0]) || (b[0]==c[0]&&b[0]<a[0])){
+        vecCopy(varyDim,c,aPrime);
+        vecCopy(varyDim,a,bPrime);
+        vecCopy(varyDim,b,cPrime);
     }
-    else if((b[0]<=a[0] && a[0]<=c[0])||
-            (b[0]<=c[0] && c[0]<=a[0])){
-        aggregateArray(varyDim,points,b,c,a);
+    else if((b[0]<a[0] && b[0]<c[0]) || (a[0]==b[0] && c[0]>a[0]) || (a[0]==c[0] && b[0]<a[0])) {
+        vecCopy(varyDim, b, aPrime);
+        vecCopy(varyDim, c, bPrime);
+        vecCopy(varyDim, a, cPrime);
     }
     else{
-        aggregateArray(varyDim,points, a,b,c);
+        vecCopy(varyDim, a, aPrime);
+        vecCopy(varyDim, b, bPrime);
+        vecCopy(varyDim, c, cPrime);
     }
 }
 
@@ -60,7 +44,7 @@ double bMinusA[],double cMinusA[]){
         {bPrime[0]-aPrime[0],cPrime[0]-aPrime[0]},
         {bPrime[1]-aPrime[1],cPrime[1]-aPrime[1]}};
     double detM = mat22Invert(M,MInv);
-    printf("%f\n",detM);
+//    printf("%f\n",detM);
     if(detM <= 0)
         return 1;
     vecSubtract(varyDim,bPrime,aPrime,bMinusA);
@@ -69,19 +53,18 @@ double bMinusA[],double cMinusA[]){
 }
 
 /* The interpolation steps that happen inside the for loops */
-void interpolate(int varyDim,double points[],double x[], double MInv[2][2],const double a[],
+void interpolate(int varyDim,double x[], double MInv[2][2],const double a[],
 double bMinusA[],double cMinusA[]){
     double pTimesBMinusA[varyDim],qTimesCMinusA[varyDim];
     double pq[2],v[2];
-    v[0] = x[0]-points[0];
-    v[1] = x[1]-points[1];
+    v[0] = x[0]-a[0];
+    v[1] = x[1]-a[1];
     mat221Multiply(MInv,v,pq);
     vecScale(varyDim,pq[0],bMinusA,pTimesBMinusA);
     vecScale(varyDim,pq[1],cMinusA,qTimesCMinusA);
-    int i;
-	for(i = 2;i <varyDim;i = i+1){
-        x[i] = a[i] + pTimesBMinusA[i] + qTimesCMinusA[i];
-    }
+//    printf("%f,%f,%f,%f,%f,%f,%f,%f\n",a[2],a[3],bMinusA[2],bMinusA[3],cMinusA[2],cMinusA[3],pq[0],pq[1]);
+    x[2] = a[2] + pTimesBMinusA[2] + qTimesCMinusA[2];
+    x[3] = a[3] + pTimesBMinusA[3] + qTimesCMinusA[3];
 }
 
 /* Assumes that the 0th and 1th elements of a, b, c are the 'x' and 'y' 
@@ -90,11 +73,9 @@ interpolate the other elements of a, b, c). */
 void triRender(const shaShading *sha, const double unif[], 
 		const texTexture *tex[], const double a[], const double b[], 
 		const double c[]){
-    //Rasterize
-    double points[sha->varyDim*3];
-    rasterize(sha->varyDim,points,a,b,c);
+    //rasterize
     double aPrime[sha->varyDim],bPrime[sha->varyDim],cPrime[sha->varyDim];
-    distributeArray(sha->varyDim,points,aPrime,bPrime,cPrime);
+    rasterize(sha->varyDim,a,b,c,aPrime,bPrime,cPrime);
 
 
     //Deal with slopes
@@ -106,7 +87,8 @@ void triRender(const shaShading *sha, const double unif[],
     double bMinusA[sha->varyDim],cMinusA[sha->varyDim];
     double MInv[2][2];
     if(prepareInterpolate(sha->varyDim,MInv,aPrime,bPrime,cPrime,bMinusA,cMinusA)!=0){
-        printf("something went wrong\n");
+//        printf("something went wrong\n");
+        return;
     }
 
     //Actually looping and interpolating 
@@ -116,14 +98,14 @@ void triRender(const shaShading *sha, const double unif[],
     //Case 1: c is left of b, top slope changes
         for (x[0]=(int)ceil(aPrime[0]); x[0]<=(int)floor(cPrime[0]);x[0]=x[0]+1) {
             for(x[1]=YCoordOf(x[0],slopeLeftRight,aPrime[0],aPrime[1]);x[1]<=YCoordOf(x[0],slopeLeftTop,aPrime[0],aPrime[1]);x[1]=x[1]+1){
-                interpolate(sha->varyDim,points,x,MInv,aPrime,bMinusA,cMinusA);
+                interpolate(sha->varyDim,x,MInv,aPrime,bMinusA,cMinusA);
                 sha->colorPixel(sha->unifDim,unif,sha->texNum,tex,sha->varyDim,x,rgb);
                 pixSetRGB((int)x[0],(int)x[1],rgb[0],rgb[1],rgb[2]);
             }
         }
         for (x[0]=(int)floor(cPrime[0])+1;x[0]<=(int)floor(bPrime[0]);x[0]=x[0]+1){
             for(x[1]=YCoordOf(x[0],slopeLeftRight,aPrime[0],aPrime[1]);x[1]<=YCoordOf(x[0],slopeRightTop,cPrime[0],cPrime[1]);x[1]=x[1]+1){
-                interpolate(sha->varyDim,points,x,MInv,aPrime,bMinusA,cMinusA);
+                interpolate(sha->varyDim,x,MInv,aPrime,bMinusA,cMinusA);
                 sha->colorPixel(sha->unifDim,unif,sha->texNum,tex,sha->varyDim,x,rgb);
                 pixSetRGB((int)x[0],(int)x[1],rgb[0],rgb[1],rgb[2]);
             }
@@ -133,14 +115,14 @@ void triRender(const shaShading *sha, const double unif[],
     //Case 2: c is right of b, bottom slope changes
         for (x[0]=(int)ceil(aPrime[0]); x[0]<=(int)floor(bPrime[0]);x[0]=x[0]+1) {
             for(x[1]=YCoordOf(x[0],slopeLeftRight,aPrime[0],aPrime[1]);x[1]<=YCoordOf(x[0],slopeLeftTop,aPrime[0],aPrime[1]);x[1]=x[1]+1){
-                interpolate(sha->varyDim,points,x,MInv,aPrime,bMinusA,cMinusA);
+                interpolate(sha->varyDim,x,MInv,aPrime,bMinusA,cMinusA);
                 sha->colorPixel(sha->unifDim,unif,sha->texNum,tex,sha->varyDim,x,rgb);
                 pixSetRGB((int)x[0],(int)x[1],rgb[0],rgb[1],rgb[2]);
             }            
         }
         for (x[0]=(int)floor(bPrime[0])+1;x[0]<=(int)floor(cPrime[0]);x[0]=x[0]+1){
             for(x[1]=YCoordOf(x[0],slopeRightTop,bPrime[0],bPrime[1]);x[1]<=YCoordOf(x[0],slopeLeftTop,cPrime[0],cPrime[1]);x[1]=x[1]+1){
-                interpolate(sha->varyDim,points,x,MInv,aPrime,bMinusA,cMinusA);
+                interpolate(sha->varyDim,x,MInv,aPrime,bMinusA,cMinusA);
                 sha->colorPixel(sha->unifDim,unif,sha->texNum,tex,sha->varyDim,x,rgb);
                 pixSetRGB((int)x[0],(int)x[1],rgb[0],rgb[1],rgb[2]);
             }
