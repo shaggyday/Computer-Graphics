44c2455583cb4ad904424516632a1881134adf9f
shaggyday <43423101+shaggyday@users.noreply.github.com>
2019-01-29 20:55:21 -0600

    Add files via upload

---
+++ 140camera.c
@@ -0,0 +1,149 @@
+
+
+
+/* This file offers a camera data type, which encompasses an isometry and a 
+projection. */
+
+
+
+/* Feel free to read from this struct's members, but don't write to them. */
+typedef struct camCamera camCamera;
+struct camCamera {
+	isoIsometry isometry;
+	double projection[6];
+	int projectionType;
+};
+
+
+
+/*** Convenience functions for isometry ***/
+
+/* Sets the camera's isometry, in a manner suitable for third-person viewing. 
+The camera is aimed at the world coordinates target. The camera itself is 
+displaced from that target by a distance rho, in the direction specified by the 
+spherical coordinates phi and theta (as in vec3Spherical). Under normal use, 
+where 0 < phi < pi, the camera's up-direction is world-up, or as close to it as 
+possible. */
+void camLookAt(camCamera *cam, const double target[3], double rho, double phi, 
+		double theta) {
+	double z[3], y[3], yStd[3] = {0.0, 1.0, 0.0}, zStd[3] = {0.0, 0.0, 1.0};
+	double rot[3][3], trans[3];
+	vec3Spherical(1.0, phi, theta, z);
+	vec3Spherical(1.0, M_PI / 2.0 - phi, theta + M_PI, y);
+	mat33BasisRotation(yStd, zStd, y, z, rot);
+	isoSetRotation(&(cam->isometry), rot);
+	vecScale(3, rho, z, trans);
+	vecAdd(3, target, trans, trans);
+	isoSetTranslation(&(cam->isometry), trans);
+}
+
+/* Sets the camera's isometry, in a manner suitable for first-person viewing. 
+The camera is positioned at the world coordinates position. From that position, 
+the camera's sight direction is described by the spherical coordinates phi and 
+theta (as in vec3Spherical). Under normal use, where 0 < phi < pi, the camera's 
+up-direction is world-up, or as close to it as possible. */
+void camLookFrom(camCamera *cam, const double position[3], double phi, 
+		double theta) {
+	double negZ[3], y[3],  yStd[3] = {0.0, 1.0, 0.0};
+	double negZStd[3] = {0.0, 0.0, -1.0}, rot[3][3];
+	vec3Spherical(1.0, phi, theta, negZ);
+	vec3Spherical(1.0, M_PI / 2.0 - phi, theta + M_PI, y);
+	mat33BasisRotation(yStd, negZStd, y, negZ, rot);
+	isoSetRotation(&(cam->isometry), rot);
+	isoSetTranslation(&(cam->isometry), position);
+}
+
+
+
+/*** Projections ***/
+
+#define camORTHOGRAPHIC 0
+#define camPERSPECTIVE 1
+#define camPROJL 0
+#define camPROJR 1
+#define camPROJB 2
+#define camPROJT 3
+#define camPROJF 4
+#define camPROJN 5
+
+/* Sets the projection type, to either camORTHOGRAPHIC or camPERSPECTIVE. */
+void camSetProjectionType(camCamera *cam, int projType) {
+	cam->projectionType = projType;
+}
+
+/* Sets all six projection parameters. */
+void camSetProjection(camCamera *cam, const double proj[6]) {
+	vecCopy(6, proj, cam->projection);
+}
+
+/* Sets one of the six projection parameters. */
+void camSetOneProjection(camCamera *cam, int i, double value) {
+	cam->projection[i] = value;
+}
+
+/* Builds a 4x4 matrix representing orthographic projection with a boxy viewing 
+volume [left, right] x [bottom, top] x [far, near]. That is, on the near plane 
+the box is the rectangle R = [left, right] x [bottom, top], and on the far 
+plane the box is the same rectangle R. Keep in mind that 0 > near > far. Maps 
+the viewing volume to [-1, 1] x [-1, 1] x [-1, 1], with far going to 1 and near 
+going to -1. */
+void camGetOrthographic(const camCamera *cam, double proj[4][4]) {
+	
+}
+
+/* Inverse to the matrix produced by camGetOrthographic. */
+void camGetInverseOrthographic(const camCamera *cam, double proj[4][4]) {
+	double left = cam->projection[camPROJL];
+	double right = cam->projection[camPROJR];
+	double bottom = cam->projection[camPROJB];
+	double top = cam->projection[camPROJT];
+	double far = cam->projection[camPROJF];
+	double near = cam->projection[camPROJN];
+	mat44Zero(proj);
+	proj[0][0] = (right - left) / 2.0;
+	proj[0][3] = (right + left) / 2.0;
+	proj[1][1] = (top - bottom) / 2.0;
+	proj[1][3] = (top + bottom) / 2.0;
+	proj[2][2] = (near - far) / -2.0;
+	proj[2][3] = (near + far) / 2.0;
+	proj[3][3] = 1.0;
+}
+
+
+
+/*** Convenience functions ***/
+
+/* Sets the six projection parameters, based on the width and height of the 
+viewport and three other parameters. The camera looks down the center of the 
+viewing volume. For perspective projection, fovy is the full (not half) 
+vertical angle of the field of vision, in radians. focal > 0 is the distance 
+from the camera to the focal plane (where 'focus' is used in the sense of 
+attention, not optics). ratio expresses the far and near clipping planes 
+relative to focal: far = -focal * ratio and near = -focal / ratio. Reasonable 
+values are fovy = M_PI / 6.0, focal = 10.0, and ratio = 10.0, so that 
+far = -100.0 and near = -1.0, but really they depend on the scene being 
+rendered. For orthographic projection, the projection parameters are set to 
+produce the orthographic projection that, at the focal plane, is most similar 
+to the perspective projection just described. You must re-invoke this function 
+after each time you change the viewport or projection type. */
+void camSetFrustum(camCamera *cam, double fovy, double focal, double ratio, 
+		double width, double height) {
+	cam->projection[camPROJF] = -focal * ratio;
+	cam->projection[camPROJN] = -focal / ratio;
+	if (cam->projectionType == camPERSPECTIVE)
+		cam->projection[camPROJT] = -cam->projection[camPROJN] 
+			* tan(fovy * 0.5);
+	else
+		cam->projection[camPROJT] = focal * tan(fovy * 0.5);
+	cam->projection[camPROJB] = -cam->projection[camPROJT];
+	cam->projection[camPROJR] = cam->projection[camPROJT] * width / height;
+	cam->projection[camPROJL] = -cam->projection[camPROJR];
+}
+
+/* Returns the homogeneous 4x4 product of the camera's projection and the 
+camera's inverse isometry. */
+void camGetProjectionInverseIsometry(camCamera *cam, double homog[4][4]) {
+	
+}
+
+
---
+++ 140mainLandscape.c
@@ -0,0 +1,246 @@
+
+
+
+/* On macOS, compile with...
+    clang 140mainLandscape.c 000pixel.o -lglfw -framework OpenGL
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <GLFW/glfw3.h>
+
+#include "000pixel.h"
+#include "120vector.c"
+#include "140matrix.c"
+#include "040texture.c"
+#include "130shading.c"
+#include "130depth.c"
+#include "130triangle.c"
+#include "130mesh.c"
+#include "140isometry.c"
+#include "140camera.c"
+#include "140landscape.c"
+
+#define mainSCREENSIZE 512
+
+/*** Shaders ***/
+
+#define mainATTRX 0
+#define mainATTRY 1
+#define mainATTRZ 2
+#define mainATTRS 3
+#define mainATTRT 4
+#define mainATTRN 5
+#define mainATTRO 6
+#define mainATTRP 7
+#define mainVARYX 0
+#define mainVARYY 1
+#define mainVARYZ 2
+#define mainVARYWORLDZ 3
+#define mainUNIFR 0
+#define mainUNIFG 1
+#define mainUNIFB 2
+#define mainUNIFMODELING 3
+#define mainUNIFMIN 4
+#define mainUNIFMEAN 5
+#define mainUNIFMAX 6
+#define mainUNIFCAMERA 7
+
+/* Solid colors, tinted from dark (low saturation at low elevation) to light 
+(high saturation at high elevation). */
+void colorPixel(int unifDim, const double unif[], int texNum, 
+		const texTexture *tex[], int varyDim, const double vary[], 
+		double rgbd[4]) {
+	double frac = (vary[mainVARYWORLDZ] - unif[mainUNIFMIN]) 
+		/ (unif[mainUNIFMAX] - unif[mainUNIFMIN]);
+	rgbd[0] = unif[mainUNIFR] * (frac + 1.0) / 2.0;
+	rgbd[1] = unif[mainUNIFG] * (frac + 1.0) / 2.0;
+	rgbd[2] = unif[mainUNIFB] * (frac + 1.0) / 2.0;
+	rgbd[3] = vary[mainVARYZ];
+}
+
+void transformVertex(int unifDim, const double unif[], int attrDim, 
+		const double attr[], int varyDim, double vary[]) {
+	double attrHom[4] = {attr[0], attr[1], attr[2], 1.0};
+	double worldHom[4], varyHom[4];
+	/* The modeling transformation is just Z-translation. So this code is much 
+	simpler than the usual matrix multiplication. */
+	vecCopy(4, attrHom, worldHom);
+	worldHom[2] += unif[mainUNIFMODELING];
+	mat441Multiply((double(*)[4])(&unif[mainUNIFCAMERA]), worldHom, varyHom);
+	vecCopy(3, varyHom, vary);
+	vary[mainVARYWORLDZ] = worldHom[2];
+}
+
+/*** Globals ***/
+
+/* Crucial infrastructure. */
+depthBuffer buf;
+shaShading sha;
+camCamera cam;
+/* Camera control. */
+double cameraTarget[3] = {0.0, 0.0, 0.0};
+double cameraRho = 256.0, cameraPhi = M_PI / 4.0, cameraTheta = 0.0;
+/* Meshes to be rendered. */
+meshMesh grass;
+double unifGrass[3 + 1 + 3 + 16] = {
+	0.0, 1.0, 0.0, 
+	0.0, 
+	0.0, 0.0, 0.0, 
+	1.0, 0.0, 0.0, 0.0, 
+	0.0, 1.0, 0.0, 0.0, 
+	0.0, 0.0, 1.0, 0.0, 
+	0.0, 0.0, 0.0, 1.0};
+meshMesh rock;
+double unifRock[3 + 1 + 3 + 16] = {
+	1.0, 1.0, 1.0, 
+	0.0, 
+	0.0, 0.0, 0.0, 
+	1.0, 0.0, 0.0, 0.0, 
+	0.0, 1.0, 0.0, 0.0, 
+	0.0, 0.0, 1.0, 0.0, 
+	0.0, 0.0, 0.0, 1.0};
+meshMesh water;
+double unifWater[3 + 1 + 3 + 16] = {
+	0.0, 0.0, 1.0, 
+	0.0, 
+	0.0, 0.0, 0.0, 
+	1.0, 0.0, 0.0, 0.0, 
+	0.0, 1.0, 0.0, 0.0, 
+	0.0, 0.0, 1.0, 0.0, 
+	0.0, 0.0, 0.0, 1.0};
+
+/*** User interface ***/
+
+void render(void) {
+	double view[4][4], projInvIsom[4][4], viewProjInvIsom[4][4];
+	camGetProjectionInverseIsometry(&cam, projInvIsom);
+	mat44Viewport(mainSCREENSIZE, mainSCREENSIZE, view);
+	mat444Multiply(view, projInvIsom, viewProjInvIsom);
+	pixClearRGB(0.0, 0.0, 0.0);
+	depthClearDepths(&buf, 1000000000.0);
+	vecCopy(16, (double *)viewProjInvIsom, &unifGrass[mainUNIFCAMERA]);
+	meshRender(&grass, &buf, &sha, unifGrass, NULL);
+	vecCopy(16, (double *)viewProjInvIsom, &unifRock[mainUNIFCAMERA]);
+	meshRender(&rock, &buf, &sha, unifRock, NULL);
+	vecCopy(16, (double *)viewProjInvIsom, &unifWater[mainUNIFCAMERA]);
+	meshRender(&water, &buf, &sha, unifWater, NULL);
+}
+
+void handleKeyAny(int key, int shiftIsDown, int controlIsDown, 
+		int altOptionIsDown, int superCommandIsDown) {
+	if (key == GLFW_KEY_A)
+		cameraTheta -= M_PI / 100;
+	else if (key == GLFW_KEY_D)
+		cameraTheta += M_PI / 100;
+	else if (key == GLFW_KEY_W)
+		cameraPhi -= M_PI / 100;
+	else if (key == GLFW_KEY_S)
+		cameraPhi += M_PI / 100;
+	else if (key == GLFW_KEY_Q)
+		cameraRho *= 0.9;
+	else if (key == GLFW_KEY_E)
+		cameraRho *= 1.1;
+	else if (key == GLFW_KEY_K)
+		cameraTarget[0] -= 0.5;
+	else if (key == GLFW_KEY_SEMICOLON)
+		cameraTarget[0] += 0.5;
+	else if (key == GLFW_KEY_L)
+		cameraTarget[1] -= 0.5;
+	else if (key == GLFW_KEY_O)
+		cameraTarget[1] += 0.5;
+	else if (key == GLFW_KEY_I)
+		cameraTarget[2] -= 0.5;
+	else if (key == GLFW_KEY_P)
+		cameraTarget[2] += 0.5;
+	else if (key == GLFW_KEY_J)
+		unifWater[mainUNIFMODELING] -= 0.1;
+	else if (key == GLFW_KEY_U)
+		unifWater[mainUNIFMODELING] += 0.1;
+	camSetFrustum(&cam, M_PI / 6.0, cameraRho, 10.0, mainSCREENSIZE, 
+		mainSCREENSIZE);
+	camLookAt(&cam, cameraTarget, cameraRho, cameraPhi, cameraTheta);
+}
+
+void handleTimeStep(double oldTime, double newTime) {
+	if (floor(newTime) - floor(oldTime) >= 1.0)
+		printf("handleTimeStep: %f frames/sec\n", 1.0 / (newTime - oldTime));
+	render();
+}
+
+int main(void) {
+	/* Design landscape and water. */
+	int landNum = 100;
+	double landData[landNum][landNum];
+	double landMin, landMean, landMax;
+	time_t t;
+	int i;
+	srand((unsigned)time(&t));
+	landFlat(landNum, landNum, (double *)landData, 0.0);
+	for (i = 0; i < 32; i += 1)
+		landFault(landNum, landNum, (double *)landData, 1.5 - i * 0.04);
+	for (i = 0; i < 4; i += 1)
+		landBlur(landNum, landNum, (double *)landData);
+	landStatistics(landNum, landNum, (double *)landData, &landMin, &landMean, 
+		&landMax);
+	double waterData[4] = {landMin, landMin, landMin, landMin};
+	unifGrass[mainUNIFMIN] = landMin;
+	unifGrass[mainUNIFMEAN] = landMean;
+	unifGrass[mainUNIFMAX] = landMax;
+	unifRock[mainUNIFMIN] = landMin;
+	unifRock[mainUNIFMEAN] = landMean;
+	unifRock[mainUNIFMAX] = landMax;
+	unifWater[mainUNIFMIN] = landMin;
+	unifWater[mainUNIFMEAN] = landMean;
+	unifWater[mainUNIFMAX] = landMax;
+	meshMesh land;
+	/* Begin configuring scene. */
+	if (pixInitialize(mainSCREENSIZE, mainSCREENSIZE, "Pixel Graphics") != 0)
+		return 1;
+	else if (depthInitialize(&buf, mainSCREENSIZE, mainSCREENSIZE) != 0)
+		return 2;
+	else if (meshInitializeLandscape(&land, landNum, landNum, 1.0, 
+			(double *)landData) != 0)
+		return 3;
+	else if (meshInitializeDissectedLandscape(&grass, &land, M_PI / 4.0, 
+			1) != 0)
+		return 4;
+	else if (meshInitializeDissectedLandscape(&rock, &land, M_PI / 4.0, 
+			0) != 0)
+		return 5;
+	else if (meshInitializeLandscape(&water, 2, 2, landNum - 1.0, 
+			(double *)waterData) != 0)
+		return 6;
+	else {
+		meshDestroy(&land);
+		/* Continue configuring scene. */
+		sha.unifDim = 3 + 1 + 3 + 16;
+		sha.attrDim = 3 + 2 + 3;
+		sha.varyDim = 4;
+		sha.colorPixel = colorPixel;
+		sha.transformVertex = transformVertex;
+		sha.texNum = 0;
+		camSetProjectionType(&cam, camORTHOGRAPHIC);
+		camSetFrustum(&cam, M_PI / 6.0, cameraRho, 10.0, mainSCREENSIZE, 
+			mainSCREENSIZE);
+		vec3Set(landNum / 2.0, landNum / 2.0, 
+			landData[landNum / 2][landNum / 2], cameraTarget);
+		camLookAt(&cam, cameraTarget, cameraRho, cameraPhi, cameraTheta);
+		/* User interface. */
+		pixSetKeyDownHandler(handleKeyAny);
+		pixSetKeyRepeatHandler(handleKeyAny);
+		pixSetKeyUpHandler(handleKeyAny);
+		pixSetTimeStepHandler(handleTimeStep);
+		pixRun();
+		/* Clean up. */
+		meshDestroy(&grass);
+		meshDestroy(&rock);
+		meshDestroy(&water);
+		depthDestroy(&buf);
+		return 0;
+	}
+}
+
+
