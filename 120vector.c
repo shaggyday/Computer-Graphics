341502a8688aedd746256f48eaafa50b1b2eb2c1
shaggyday <43423101+shaggyday@users.noreply.github.com>
2019-02-07 16:14:19 -0600

    Add files via upload

---
+++ 120vector.c
@@ -0,0 +1,154 @@
+//Harry Tian
+
+
+/*** In general dimensions ***/
+
+void vecClear3(int dim, double v[] , double w[], double u[]){
+    for (int i = 0; i < dim; i += 1) {
+        v[i] = 0.0;
+        w[i] = 0.0;
+        u[i] = 0.0;
+    }
+}
+
+/* Copies the dim-dimensional vector v to the dim-dimensional vector copy. The 
+output can safely alias the input. */
+void vecCopy(int dim, const double v[], double copy[]) {
+	for (int i = 0; i < dim; i += 1)
+		copy[i] = v[i];
+}
+
+/* Pretty-prints the given matrix, with one line of text per row of matrix. */
+void vecPrint(int dim, const double v[]) {
+	for (int i = 0; i < dim; i += 1) {
+		printf("%f\n", v[i]);
+	}
+	printf("\n");
+}
+
+/* Adds the dim-dimensional vectors v and w. The output can safely alias the 
+input. */
+void vecAdd(int dim, const double v[], const double w[], double vPlusW[]) {
+	for(int i = 0;i < dim;i = i+1){
+		vPlusW[i] = v[i] + w[i];
+	}
+}
+
+/* Adds the dim-dimensional vectors v,w and u. The output can safely alias the 
+input. */
+void vecAdd3(int dim, const double v[], const double w[], const double u[], double vPlusW[]) {
+	for(int i = 0;i < dim;i += 1){
+		vPlusW[i] = v[i] + w[i] + u[i];
+	}
+}
+
+/* Subtracts the dim-dimensional vectors v and w. The output can safely alias 
+the input. */
+void vecSubtract(int dim, const double v[], const double w[], 
+		double vMinusW[]) {
+	for(int i = 0;i < dim;i += 1){
+		vMinusW[i] = v[i] - w[i];
+	}
+}
+
+/* Scales the dim-dimensional vector w by the number c. The output can safely 
+alias the input.*/
+void vecScale(int dim, double c, const double w[], double cTimesW[]) {
+	for(int i = 0;i < dim;i += 1){
+		cTimesW[i] = w[i]*c;
+	}	
+}
+
+/* Returns the dot product of the vectors v and w. */
+double vecDot(int dim, const double v[], const double w[]){
+	double dot;
+	for(int i = 0;i < dim;i += 1)
+		dot += v[i]*w[i];
+	return dot;
+}
+
+/* Returns the "multiply product" of the vectors v and w. */
+void vecMultiply(int dim, const double v[], const double w[], double vTimesw[]){
+    for(int i = 0; i < dim; i += 1){
+        vTimesw[i] = v[i]*w[i];
+    }
+}
+
+/* Returns the length of the vector v. */
+double vecLength(int dim, const double v[]){
+	double length;
+	for(int i = 0;i < dim;i += 1)
+		length += v[i]*v[i];
+	return sqrt(length);
+}
+
+/* Returns the length of the vector v. If the length is non-zero, then also
+places a normalized (length-1) version of v into unit. The output can safely
+alias the input. */
+double vecUnit(int dim, const double v[], double unit[]){
+	double length = vecLength(dim,v);
+	if(length != 0)
+		vecScale(dim,1/length,v,unit);
+	return length;
+}
+
+/* Computes the cross product of v and w, and places it into vCrossW. The
+output CANNOT safely alias the input. */
+void vec3Cross(const double v[3], const double w[3], double vCrossW[3]){
+	vCrossW[0] = v[1]*w[2] - v[2]*w[1];
+	vCrossW[1] = v[2]*w[0] - v[0]*w[2];
+	vCrossW[2] = v[0]*w[1] - v[1]*w[0];
+}
+
+/* Computes the vector v from its spherical coordinates. rho >= 0.0 is the
+radius. 0 <= phi <= pi is the co-latitude. -pi <= theta <= pi is the longitude
+or azimuth. */
+void vec3Spherical(double rho, double phi, double theta, double v[3]){
+	double sinPhi = sin(phi);
+	v[0] = rho*sinPhi*cos(theta);
+	v[1] = rho*sinPhi*sin(theta);
+	v[2] = rho*cos(phi);
+}
+
+
+/*** In specific dimensions ***/
+
+/* By the way, there is a way to write a single vecSet function that works in 
+all dimensions. The module stdarg.h, which is part of the C standard library, 
+lets you write variable-arity functions. The general vecSet would look like
+	void vecSet(int dim, double a[], ...)
+where the '...' represents dim numbers to be loaded into a. We're not going to 
+take this approach for two reasons. First, I try not to burden you with 
+learning a lot of C that isn't strictly necessary. Second, the variable-arity 
+feature is a bit dangerous, in that it provides no type checking. */
+
+/* Copies three numbers into a three-dimensional vector. */
+void vec3Set(double a0, double a1, double a2, double a[3]) {
+	a[0] = a0;
+	a[1] = a1;
+	a[2] = a2;
+}
+
+
+/* Copies four numbers into a four-dimensional vector. */
+void vec4Set(double a0, double a1, double a2, double a3, double a[4]) {
+	a[0] = a0;
+	a[1] = a1;
+	a[2] = a2;
+	a[3] = a3;	
+}
+
+/* Copies eight numbers into a eight-dimensional vector. */
+void vec8Set(double a0, double a1, double a2, double a3, double a4, double a5, 
+		double a6, double a7, double a[8]) {
+	a[0] = a0;
+	a[1] = a1;
+	a[2] = a2;
+	a[3] = a3;	
+	a[4] = a4;
+	a[5] = a5;
+	a[6] = a6;
+	a[7] = a7;		
+}
+
+
---
+++ 180mainDiffuse.c
@@ -0,0 +1,292 @@
+
+
+
+/* On macOS, compile with...
+    clang 150mainPerspective.c 000pixel.o -lglfw -framework OpenGL
+*/
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <GLFW/glfw3.h>
+
+#include "170engine.h"
+
+#define mainSCREENSIZE 512
+
+/*** Shaders ***/
+
+#define mainATTRX 0
+#define mainATTRY 1
+#define mainATTRZ 2
+#define mainATTRS 3
+#define mainATTRT 4
+#define mainATTRN 5
+#define mainATTRO 6
+#define mainATTRP 7
+#define mainVARYX 0
+#define mainVARYY 1
+#define mainVARYZ 2
+#define mainVARYW 3
+#define mainVARYWORLDZ 4
+#define mainVARYN 5
+#define mainVARYO 6
+#define mainVARYP 7
+#define mainUNIFR 0
+#define mainUNIFG 1
+#define mainUNIFB 2
+#define mainUNIFMODELING 3
+#define mainUNIFMIN 4
+#define mainUNIFMEAN 5
+#define mainUNIFMAX 6
+#define mainUNIFCAMERA 7
+#define mainUNIFcLIGHT 23
+#define mainUNIFdLIGHT 26
+#define mainuNIFdCamera 29
+
+/* Solid colors, tinted from dark (low saturation at low elevation) to light 
+(high saturation at high elevation). */
+void colorPixel(int unifDim, const double unif[], int texNum, 
+		const texTexture *tex[], int varyDim, const double vary[], 
+		double rgbd[4]) {
+	double frac = (vary[mainVARYWORLDZ] - unif[mainUNIFMIN])
+		/ (unif[mainUNIFMAX] - unif[mainUNIFMIN]);
+	double cDiffuse[3];
+    cDiffuse[mainUNIFR] = unif[mainUNIFR] * (frac + 1.0) / 2.0;
+    cDiffuse[mainUNIFG] = unif[mainUNIFG] * (frac + 1.0) / 2.0;
+    cDiffuse[mainUNIFB] = unif[mainUNIFB] * (frac + 1.0) / 2.0;
+	/* Lambertian diffuse reflection */
+    vecMultiply(3,cDiffuse,&unif[mainUNIFcLIGHT],rgbd);
+	vecCopy(3, cDiffuse, rgbd);
+	double dNormal[3] = {vary[mainVARYN],vary[mainVARYO],vary[mainVARYP]};
+	double dNormalUnit[3];
+	vecUnit(3,dNormal,dNormalUnit);
+	double intensity = vecDot(3, dNormalUnit, &unif[mainUNIFdLIGHT]);
+	if (intensity < 0.0)
+	    intensity = 0.0;
+	vecScale(3, intensity, rgbd, rgbd);
+    rgbd[3] = vary[mainVARYZ];
+}
+
+void transformVertex(int unifDim, const double unif[], int attrDim, 
+		const double attr[], int varyDim, double vary[]) {
+	double attrHom[4] = {attr[mainATTRX], attr[mainATTRY], attr[mainATTRZ], 1.0};
+	double worldHom[4], varyHom[4], varyNormalHom[4];
+	/* The modeling transformation is just Z-translation. So this code is much 
+	simpler than the usual matrix multiplication. */
+	vecCopy(4, attrHom, worldHom);
+	worldHom[2] += unif[mainUNIFMODELING];
+	mat441Multiply((double(*)[4])(&unif[mainUNIFCAMERA]), worldHom, varyHom);
+	vecCopy(4, varyHom, vary);
+	vary[mainVARYWORLDZ] = worldHom[mainATTRZ];
+	/* Rotating but not translating normal vector varyings */
+	double normalHom[4] = {attr[mainATTRN],attr[mainATTRO],attr[mainATTRP],0.0};
+	mat441Multiply((double(*)[4])(&unif[mainUNIFCAMERA]), normalHom, varyNormalHom);
+	vary[mainVARYN] = varyNormalHom[0];
+	vary[mainVARYO] = varyNormalHom[1];
+	vary[mainATTRP] = varyNormalHom[2];
+}
+
+/*** Globals ***/
+
+/* Crucial infrastructure. */
+depthBuffer buf;
+shaShading sha;
+camCamera cam;
+/* Camera control. */
+double cameraTarget[3] = {0.0, 0.0, 0.0};
+double cameraRho = 256.0, cameraPhi = M_PI / 4.0, cameraTheta = 0.0;
+/* Meshes to be rendered. */
+meshMesh grass;
+double unifGrass[3 + 1 + 3 + 16 + 3 + 3 + 3] = {
+	0.0, 1.0, 0.0,
+	0.0,
+	0.0, 0.0, 0.0,
+	1.0, 0.0, 0.0, 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0,
+    1.0, 1.0, 1.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0};
+meshMesh rock;
+double unifRock[3 + 1 + 3 + 16 + 3 + 3 + 3] = {
+	1.0, 1.0, 1.0,
+	0.0,
+	0.0, 0.0, 0.0,
+	1.0, 0.0, 0.0, 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0,
+	1.0, 1.0, 1.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0};
+meshMesh water;
+double unifWater[3 + 1 + 3 + 16 + 3 + 3 + 3] = {
+	0.0, 0.0, 1.0,
+	0.0,
+	0.0, 0.0, 0.0,
+	1.0, 0.0, 0.0, 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0,
+    1.0, 1.0, 1.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0};
+double dLight[3] = {512.0, 512.0, 512.0};
+
+/*** User interface ***/
+
+void render(void) {
+	double view[4][4], projInvIsom[4][4];
+	camGetProjectionInverseIsometry(&cam, projInvIsom);
+	mat44Viewport(mainSCREENSIZE, mainSCREENSIZE, view);
+	pixClearRGB(0.0, 0.0, 0.0);
+	depthClearDepths(&buf, 1000000000.0);
+	vecCopy(16, (double *)projInvIsom, &unifGrass[mainUNIFCAMERA]);
+	meshRender(&grass, &buf, view, &sha, unifGrass, NULL);
+	vecCopy(16, (double *)projInvIsom, &unifRock[mainUNIFCAMERA]);
+	meshRender(&rock, &buf, view, &sha, unifRock, NULL);
+	vecCopy(16, (double *)projInvIsom, &unifWater[mainUNIFCAMERA]);
+	meshRender(&water, &buf, view, &sha, unifWater, NULL);
+}
+
+void handleKeyAny(int key, int shiftIsDown, int controlIsDown, 
+		int altOptionIsDown, int superCommandIsDown) {
+	if (key == GLFW_KEY_A)
+		cameraTheta -= M_PI / 100;
+	else if (key == GLFW_KEY_D)
+		cameraTheta += M_PI / 100;
+	else if (key == GLFW_KEY_W)
+		cameraPhi -= M_PI / 100;
+	else if (key == GLFW_KEY_S)
+		cameraPhi += M_PI / 100;
+	else if (key == GLFW_KEY_Q)
+		cameraRho *= 0.9;
+	else if (key == GLFW_KEY_E)
+		cameraRho *= 1.1;
+	else if (key == GLFW_KEY_Z)
+		cameraTarget[0] -= 5;
+	else if (key == GLFW_KEY_C)
+		cameraTarget[0] += 5;
+	else if (key == GLFW_KEY_RIGHT)
+		cameraTarget[1] -= 1;
+	else if (key == GLFW_KEY_LEFT)
+		cameraTarget[1] += 1;
+	else if (key == GLFW_KEY_UP)
+		cameraTarget[2] -= 1;
+	else if (key == GLFW_KEY_DOWN)
+		cameraTarget[2] += 1;
+	else if (key == GLFW_KEY_O)
+		unifWater[mainUNIFMODELING] -= 0.1;
+	else if (key == GLFW_KEY_P)
+		unifWater[mainUNIFMODELING] += 0.1;
+	camSetFrustum(&cam, M_PI / 6.0, cameraRho, 10.0, mainSCREENSIZE, 
+		mainSCREENSIZE);
+	camLookAt(&cam, cameraTarget, cameraRho, cameraPhi, cameraTheta);
+}
+
+void handleKeyUp(int key, int shiftIsDown, int controlIsDown,
+				  int altOptionIsDown, int superCommandIsDown) {
+	if (key == GLFW_KEY_ENTER) {
+		if (cam.projectionType == camORTHOGRAPHIC)
+			camSetProjectionType(&cam, camPERSPECTIVE);
+		else if(cam.projectionType == camPERSPECTIVE)
+			camSetProjectionType(&cam,camORTHOGRAPHIC);
+		camSetFrustum(&cam, M_PI / 6.0, cameraRho, 10.0, mainSCREENSIZE,
+					  mainSCREENSIZE);
+		camLookAt(&cam, cameraTarget, cameraRho, cameraPhi, cameraTheta);
+	}
+}
+
+void handleTimeStep(double oldTime, double newTime) {
+	if (floor(newTime) - floor(oldTime) >= 1.0)
+		printf("handleTimeStep: %f frames/sec\n", 1.0 / (newTime - oldTime));
+	render();
+}
+
+int main(void) {
+	/* Design landscape and water. */
+	int landNum = 100;
+	double landData[landNum][landNum];
+	double landMin, landMean, landMax;
+	time_t t;
+	int i;
+	srand((unsigned)time(&t));
+	landFlat(landNum, landNum, (double *)landData, 0.0);
+	for (i = 0; i < 32; i += 1)
+		landFault(landNum, landNum, (double *)landData, 1.5 - i * 0.04);
+	for (i = 0; i < 4; i += 1)
+		landBlur(landNum, landNum, (double *)landData);
+	landStatistics(landNum, landNum, (double *)landData, &landMin, &landMean, 
+		&landMax);
+	double waterData[4] = {landMin, landMin, landMin, landMin};
+	unifGrass[mainUNIFMIN] = landMin;
+	unifGrass[mainUNIFMEAN] = landMean;
+	unifGrass[mainUNIFMAX] = landMax;
+	unifRock[mainUNIFMIN] = landMin;
+	unifRock[mainUNIFMEAN] = landMean;
+	unifRock[mainUNIFMAX] = landMax;
+	unifWater[mainUNIFMIN] = landMin;
+	unifWater[mainUNIFMEAN] = landMean;
+	unifWater[mainUNIFMAX] = landMax;
+	double dLightUnit[3];
+	vecUnit(3, dLight, dLightUnit);
+	vecCopy(3, dLightUnit, &unifGrass[mainUNIFdLIGHT]);
+    vecCopy(3, dLightUnit, &unifRock[mainUNIFdLIGHT]);
+    vecCopy(3, dLightUnit, &unifWater[mainUNIFdLIGHT]);
+	meshMesh land;
+	/* Begin configuring scene. */
+	if (pixInitialize(mainSCREENSIZE, mainSCREENSIZE, "Pixel Graphics") != 0)
+		return 1;
+	else if (depthInitialize(&buf, mainSCREENSIZE, mainSCREENSIZE) != 0)
+		return 2;
+	else if (meshInitializeLandscape(&land, landNum, landNum, 1.0, 
+			(double *)landData) != 0)
+		return 3;
+	else if (meshInitializeDissectedLandscape(&grass, &land, M_PI / 4.0, 
+			1) != 0)
+		return 4;
+	else if (meshInitializeDissectedLandscape(&rock, &land, M_PI / 4.0, 
+			0) != 0)
+		return 5;
+	else if (meshInitializeLandscape(&water, 2, 2, landNum - 1.0, 
+			(double *)waterData) != 0)
+		return 6;
+	else {
+		meshDestroy(&land);
+		/* Continue configuring scene. */
+		sha.unifDim = 3 + 1 + 3 + 16 + 3 + 3 + 3;
+		sha.attrDim = 3 + 2 + 3;
+		sha.varyDim = 4 + 1 + 2;
+		sha.colorPixel = colorPixel;
+		sha.transformVertex = transformVertex;
+		sha.texNum = 0;
+		camSetProjectionType(&cam, camORTHOGRAPHIC);
+		camSetFrustum(&cam, M_PI / 6.0, cameraRho, 10.0, mainSCREENSIZE, 
+			mainSCREENSIZE);
+		vec3Set(landNum / 2.0, landNum / 2.0, 
+			landData[landNum / 2][landNum / 2], cameraTarget);
+		camLookAt(&cam, cameraTarget, cameraRho, cameraPhi, cameraTheta);
+//		vec3Set(cameraTarget[0] + cameraRho, cameraTarget[1] + cameraRho, cameraTarget[2] + cameraRho, &unifGrass[mainuNIFdCamera]);
+//        vec3Set(cameraTarget[0] + cameraRho, cameraTarget[1] + cameraRho, cameraTarget[2] + cameraRho, &unifWater[mainuNIFdCamera]);
+//        vec3Set(cameraTarget[0] + cameraRho, cameraTarget[1] + cameraRho, cameraTarget[2] + cameraRho, &unifRock[mainuNIFdCamera]);
+		render();
+		/* User interface. */
+		pixSetKeyDownHandler(handleKeyAny);
+		pixSetKeyRepeatHandler(handleKeyAny);
+		pixSetKeyUpHandler(handleKeyAny);
+		pixSetKeyUpHandler(handleKeyUp);
+		pixSetTimeStepHandler(handleTimeStep);
+		pixRun();
+		/* Clean up. */
+		meshDestroy(&grass);
+		meshDestroy(&rock);
+		meshDestroy(&water);
+		depthDestroy(&buf);
+		return 0;
+	}
+}
+
+
